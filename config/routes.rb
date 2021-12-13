fdefdc67e992947abcd1e755b0fd377a8cc41db0
definoob <noahjun511@gmail.com>
2021-12-13 09:33:02 +0800

    Unauthenticated block

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  unauthenticated :user do
+    root 'pages#home'
+  end
+
   authenticated :user do
     root "pages#my_todo_items", as: :authenticated_root
-  end
-  root 'pages#home'
+  end  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
