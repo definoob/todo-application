5b3ce77aee6568f32cd980f8b526cb8930ac59a3
definoob <noahjun511@gmail.com>
2021-12-13 11:13:13 +0800

    Create api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   
   unauthenticated :user do
@@ -7,6 +8,11 @@ Rails.application.routes.draw do
 
   authenticated :user do
     root "pages#my_todo_items", as: :authenticated_root
-  end  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :todo_items, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
 end
