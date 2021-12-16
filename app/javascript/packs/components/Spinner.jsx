ba14452551dba0c800a3bb50b94d2e8123b1c07f
definoob <noahjun511@gmail.com>
2021-12-16 11:18:51 +0800

    modify spinner

--- app/javascript/packs/components/Spinner.jsx
+++ app/javascript/packs/components/Spinner.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Spinner = () => (
   <div className="d-flex align-items-center justify-content-center py-5">
     <div className="spinner-border" role="status">
-      <span className="sr-only">Loading...</span>
+      <span className="sr-only" />
     </div>
   </div>
 );
